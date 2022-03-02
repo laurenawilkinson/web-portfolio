146a6a340c72454fc6b0b43b58ce041387d75604
laurenawilkinson <lauren.a.wilkinson@outlook.com>
2022-03-02 21:00:38 +0000

    Add About section, Add Contact section

--- src/components/sections/About.js
+++ src/components/sections/About.js
@@ -1,13 +1,44 @@
 import * as React from 'react'
 import { containerMd } from '../../styles/utils/containers.module.scss'
-import { headingLg } from '../../styles/utils/typography.module.scss'
+import {
+  headingLg,
+  primaryText,
+} from '../../styles/utils/typography.module.scss'
 import * as styles from '../../styles/sections/About.module.scss'
 
 const About = () => {
   return (
     <section id="about" className={styles.section}>
       <div className={containerMd}>
-        <h2 className={headingLg}>About</h2>
+        <div className={styles.card}>
+          <h2 className={headingLg}>About me</h2>
+
+          <p>
+            Hi! I'm <strong className={primaryText}>Lauren</strong> and I enjoy
+            building beautiful user interfaces that are both intuitive and
+            pleasant to use. I am a Web Development graduate with{' '}
+            <strong className={primaryText}>First Class Honours</strong>, and
+            over <strong className={primaryText}>three years</strong> of
+            professional experience working within the front-end space,
+            currently based in Manchester, England.
+          </p>
+
+          <p>
+            I find the most comfort in working with{' '}
+            <strong className={primaryText}>Vue</strong> and{' '}
+            <strong className={primaryText}>Sass</strong>, but I enjoy branching
+            out and learning new technologies. Recently I have been trying out{' '}
+            <strong className={primaryText}>Next.js</strong> (with CSS Modules +
+            TypeScript) and back-end tech (Postgres with Node.js + Prisma).
+          </p>
+
+          <p>
+            Outside of coding, I enjoy playing{' '}
+            <strong className={primaryText}>video games</strong>, listening to{' '}
+            <strong className={primaryText}>podcasts</strong> and{' '}
+            <strong className={primaryText}>drawing</strong>.
+          </p>
+        </div>
       </div>
     </section>
   )
--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,15 +1,53 @@
 import * as React from 'react'
-import { containerMd } from '../../styles/utils/containers.module.scss'
+import cn from 'classnames'
+import { containerLg } from '../../styles/utils/containers.module.scss'
 import { headingLg } from '../../styles/utils/typography.module.scss'
+import {
+  buttonPlain,
+  hasIconLeft,
+} from '../../styles/components/Button.module.scss'
 import * as styles from '../../styles/sections/Contact.module.scss'
+import Icon from '../utils/Icon'
 
 const Contact = () => {
+  const links = [
+    {
+      name: 'email',
+      link: 'mailto:lauren.a.wilkinson@outlook.com',
+    },
+    { name: 'github', link: 'https://github.com/laurenawilkinson' },
+    { name: 'linkedin', link: 'https://www.linkedin.com/in/lauren-wilkinson/' },
+  ]
+
   return (
-    <section id="contact" className={styles.section}>
-      <div className={containerMd}>
-        <h2 className={headingLg}>Contact</h2>
+    <footer id="contact" className={styles.section}>
+      <div className={cn(containerLg, styles.container)}>
+        <div>
+          <h2 className={cn(headingLg, styles.heading)}>Contact</h2>
+          <p className={styles.textContainer}>
+            I'm always looking to connect and collaborate with others to build
+            amazing things. Feel free to get in touch!
+          </p>
+        </div>
+
+        <ul className={styles.links}>
+          {links.map(({ link, name, icon, image }, i) => (
+            <li className={styles.link} key={i}>
+              <a
+                className={cn(buttonPlain, hasIconLeft, styles.buttonLink)}
+                href={link}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {icon && <Icon>{icon}</Icon>}
+                {image && <img src={image} alt="" />}
+                {name}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
-    </section>
+    </footer>
   )
 }
 

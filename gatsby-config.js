e73674c9e0a537911a2a1dc580ce8b89ff969166
laurenawilkinson <lauren.a.wilkinson@outlook.com>
2022-03-18 19:34:51 +0000

    Add window svg to hero

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://laurenwilkinson.co.uk`,
   },
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /svg/,
+        },
+      },
+    },
+  ],
 }
--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import cn from 'classnames'
-import { containerLg } from '../../styles/utils/containers.module.scss'
+import { containerXl } from '../../styles/utils/containers.module.scss'
 import { headingXl } from '../../styles/utils/typography.module.scss'
 import {
   buttonMain,
@@ -8,22 +8,34 @@ import {
 } from '../../styles/components/Button.module.scss'
 import * as styles from '../../styles/sections/Hero.module.scss'
 import Icon from '../utils/Icon'
+import WindowSvg from '../../assets/svg/window.svg'
 
 const Hero = () => {
   return (
     <section className={styles.section}>
-      <div className={containerLg}>
-        <h1 className={headingXl}>Lauren Wilkinson</h1>
+      <div className={cn(containerXl, styles.container)}>
+        <div className={styles.textContainer}>
+          <h1 className={headingXl}>Lauren Wilkinson</h1>
 
-        <p className={styles.tagline}>
-          Front-End Developer with a focus on delivering aesthetic yet
-          accessible user experiences{' '}
-          <img src="/images/cherry-blossom-icon.png" alt="" height="24" />
-        </p>
+          <p className={styles.tagline}>
+            Front-End Developer with a focus on delivering aesthetic yet
+            accessible user experiences{' '}
+            <img
+              src="/images/cherry-blossom-icon.png"
+              alt=""
+              height="24"
+              width="24"
+            />
+          </p>
 
-        <a className={cn(buttonMain, hasIconRight)} href="#projects">
-          View Projects <Icon>arrow_right_alt</Icon>
-        </a>
+          <a className={cn(buttonMain, hasIconRight)} href="#projects">
+            View Projects <Icon>arrow_right_alt</Icon>
+          </a>
+        </div>
+
+        <div className={styles.imageContainer}>
+          <WindowSvg />
+        </div>
       </div>
     </section>
   )
